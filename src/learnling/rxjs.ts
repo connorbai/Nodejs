7ffa9eff2150db6330ecd306f102720b5dae30c9
connor.bai <connor.bai@innocellence.com>
2025-09-24 09:31:07 +0800

    add log file

--- src/learnling/rxjs.ts
+++ src/learnling/rxjs.ts
@@ -1,4 +1,4 @@
-import { mapTo, map, tap, concatAll, mergeAll, pluck, zipAll, combineAll, reduce, 
+import { mapTo, map, tap, concatAll, mergeAll, pluck, zipAll, combineAll, reduce,
     toArray, expand, switchMap, switchMapTo, concatMap, filter,takeWhile,
     bufferTime
 } from 'rxjs/operators';
@@ -8,7 +8,6 @@ import { interval } from 'rxjs';
 
 export async function main() {
     const source = interval(1000);
-    // 在2秒的时间间隔内缓冲发出的值，并将这些值作为数组发出
     const buffered = source.pipe(bufferTime(12000));
     buffered.subscribe((val) => console.log(val));
 }
